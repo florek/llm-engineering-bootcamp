f31a18581f80fe29ee658a3caf3761da0acf3e2e
Florek <pawel.florczak87@gmail.com>
2026-06-03 17:27:52 +0200

    2026.06.03

---
+++ src/p1.py
@@ -0,0 +1,20 @@
+import requests
+from openai import OpenAI
+
+
+print(requests.get("http://localhost:11434").content)
+
+OLLAMA_BASE_URL = "http://localhost:11434/v1"
+
+ollama = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
+
+response = ollama.chat.completions.create(
+    model='llama3.2',
+    messages=[
+        {
+            "role": "user",
+            "content": "Tell me a fun fact"
+        }
+    ]
+)
+print(response.choices[0].message.content)
